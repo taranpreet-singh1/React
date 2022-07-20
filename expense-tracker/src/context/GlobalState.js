e254b9ea8054bea8b10136c5e62e3c722367bdcb
Taranpreet Singh <taran99.singh@gmail.com>
2022-07-19 21:11:50 -0400

    Expense Tracker app and React practice

--- expense-tracker/src/context/GlobalState.js
+++ expense-tracker/src/context/GlobalState.js
@@ -3,10 +3,7 @@ import AppReducer from './AppReducer';
 
 //inital state
 const initalState = {
-    transactions: [{ id: 1, text: 'Flower', amount: -20 },
-       { id: 2, text: 'Salary', amount: 300 },
-      { id: 3, text: 'Book', amount: -10 },
-       { id: 4, text: 'Camera', amount: 150 }
+    transactions: [
 ]
 }
 
@@ -18,8 +15,28 @@ export const GlobalContext = createContext(initalState);
 export const GlobalProvider = ({children})=>{
     const [state, dispatch] = useReducer(AppReducer, initalState);
 
+
+    //Actions
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+
     return (<GlobalContext.Provider value={({
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
     })}>
         {children}
         </GlobalContext.Provider>
---
+++ single_page_react/index.js
@@ -0,0 +1,71 @@
+import React from "react"
+import ReactDom from "react-dom/client"
+//ReactDOM.render(<p>Hello, everyone</p>, document.getElementById("root"))
+
+/*
+function SomeOther(){
+    return(
+        <p>Something</p>
+    )
+}
+
+function MainContent(){
+    return(
+        <h1>I'm learning React</h1>
+    )
+}
+
+const someData = (
+    <div>
+        <h1 className="someData">Some Data</h1>
+        <p> A paragraph</p>
+    </div>
+)
+
+const navBar = (
+    <nav>
+        <h1>My Website!</h1>
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+function navBarAgain(){
+   return ( <nav>
+        <h1>My Website!</h1>
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+   )
+}
+
+/*
+ReactDOM.render(
+   //navBar,
+    <div>
+        <SomeOther />
+        <navBarAgain />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)*/
+
+//ReactDOM.createRoot(document.getElementById("root")).render(navBar)
+
+
+//Do the above mentioned task using Vanilla JavaScript only
+/*
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way"
+h1.className = "header"
+document.getElementById("root").append(h1)
+*/
+
+// JSX - JavaScript XML - Inclusion of JSX made React declarative
+
